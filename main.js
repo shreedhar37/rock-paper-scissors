f0111d73e7e159dbb63f7e0b5ea25e3e214cc4a4
Shreedhar Chavan <meshree4@gmail.com>
2022-08-28 18:48:51 +0530

    Initial Commit

---
+++ main.js
@@ -0,0 +1,37 @@
+// let userChoice = Number(
+//   prompt("Choose from below:\n1.Rock\n2.Paper\n3.Scissors\n")
+// );
+
+// console.log("You choosed : " + returnChoice(userChoice));
+
+// let computerChoice = Math.floor(Math.random() * 3 + 1);
+// console.log("Computer choosed: " + returnChoice(computerChoice));
+
+// if (userChoice === computerChoice) {
+//   console.log("Tie !!!");
+// } else if (
+//   (userChoice === 1 && computerChoice === 3) ||
+//   (userChoice === 2 && computerChoice === 1) ||
+//   (userChoice === 3 && computerChoice === 2)
+// ) {
+//   console.log("Congrats you won !!!");
+// } else if (
+//   (userChoice === 1 && computerChoice === 2) ||
+//   (userChoice === 2 && computerChoice === 3) ||
+//   (userChoice === 3 && computerChoice === 1)
+// ) {
+//   console.log("Opps you lost !!!");
+// }
+
+// function returnChoice(n) {
+//   switch (n) {
+//     case 1:
+//       return "Rock";
+
+//     case 2:
+//       return "Paper";
+
+//     case 3:
+//       return "Scissors";
+//   }
+// }
